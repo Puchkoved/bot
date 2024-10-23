0b7825b73e9418e492345552c0582a2ef9e304bd
pawlo <pawlowdanil@yandex.ru>
2024-10-23 18:16:08 +0300

    Хендлеры обработки сообщений

--- bot1.py
+++ bot1.py
@@ -2,7 +2,7 @@ from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
 from aiogram.types import Message
 
-BOT_TOKEN = '7373643217:AAF4gysa7xKtkRkDuupkWbtJEmkGFNy2s_w'
+BOT_TOKEN = ''
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
